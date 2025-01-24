5633c9aef3d826bbe46a31f4ea2ddad0b2e7caf1
radadiyasiddharth <radadiyasiddharth994@gmail.com>
2025-01-24 09:59:21 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -40,43 +40,36 @@ const DBconnection = require("./DBconnection/DBconnection");
 const router = require("./routes");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
 
 const app = express();
+
+// Middleware
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
-app.use(bodyParser.json());
 app.use(cookieParser());
 
-const allowedOrigins = [
-  "*"
-]
+// CORS Configuration
+const allowedOrigins = ["*"];
 const corsOptions = {
-  origin:function(origin,callback){
-    if(allowedOrigins.indexOf(origin) !== -1 || !origin){
-      callback(null,true);
-    }else{
-      callback(new Error('Not AllowBy CORS'))
+  origin: function (origin, callback) {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
     }
   },
-  credentials:true,
-}
-app.use(cors(corsOptions))
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  next();
-});
+// Database Connection
+DBconnection();
 
+// Routes
+app.use("/v1", router);
+
+// Server Start
 const port = 5500;
-
 app.listen(port, () => {
   console.log("Server started on port", port);
 });
-
-app.use(express.json());
-DBconnection();
-app.use("/v1", router);
