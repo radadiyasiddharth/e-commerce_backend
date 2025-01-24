756c22bfe79735ba693b45164ad6e641ab78ad38
radadiyasiddharth <radadiyasiddharth994@gmail.com>
2025-01-24 10:26:17 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -47,27 +47,14 @@ const app = express();
 // Middleware
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
+
+app.use(cors({
+  origin: true,  // Adjust according to your needs
+  credentials: true
+}));
 app.use(cookieParser());
 
-const allowedOrigins = ["https://e-commerce-fronted.onrender.com","*"]; // Add your frontend URL here
-app.use(
-  cors({
-    origin: "*",
-    credentials: true, // Allow cookies to be sent
-  })
-);
 
-// Optional: Custom middleware for dynamic CORS headers
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  next();
-});
 
 
 // Database Connection
