b212113f64e85a795a3ed9a43ed0ad7769d07d3f
radadiyasiddharth <radadiyasiddharth994@gmail.com>
2025-01-24 10:14:18 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -49,7 +49,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
 
-const allowedOrigins = ["https://e-commerce-fronted.onrender.com/"]; // Add your frontend URL here
+const allowedOrigins = ["https://e-commerce-fronted.onrender.com","*"]; // Add your frontend URL here
 app.use(
   cors({
     origin: allowedOrigins,
